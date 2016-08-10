7ddee389550497eaa02c587ad0d2c4a5bcb37113
sohjunjie <junjie.soh93@gmail.com>
2016-08-10 21:35:36 +0800

    changed username param to user_id as used by ntulearn

--- ntudownloader.py
+++ ntudownloader.py
@@ -6,7 +6,7 @@ from settings import NTULEARN_URL
 # insert loginmethod here
 def ntu_login(username, password):
 
-    values = {'username': username, 'password': password}
+    values = {'user_id': username, 'password': password}
 
     s = requests.Session()
     s.post(NTULEARN_URL, data=values)
